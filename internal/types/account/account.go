4489a80758a2cc7543ab8038ac50b32b917e360e
jsirianni <joe.sirianni@bluemedora.com>
2020-12-10 18:33:33 -0500

    ommit fields not required for safe unmarshalling of empty UUID fields

--- internal/types/account/account.go
+++ internal/types/account/account.go
@@ -8,20 +8,20 @@ import (
 
 type Account struct {
     // Unique identifier for  the account
-    AccountID  uuid.UUID `db:"account_id" json:"account_id"`
+    AccountID  uuid.UUID `db:"account_id" json:"account_id,omitempty"`
 
     // API Key with full permission to the account
-    RootAPIKey uuid.UUID `db:"root_api_key" json:"root_api_key"`
+    RootAPIKey uuid.UUID `db:"root_api_key" json:"root_api_key,omitempty"`
 
     // The accounts alert type (slack, email, etc)
-    AlertType  string    `db:"alert_type" json:"alert_type"`
+    AlertType  string    `db:"alert_type" json:"alert_type,omitempty"`
 
     // AlertConfig represents arbitrary json that will be marshalled
     // into the given alert type's configuration
-    AlertConfig alertConfig `db:"alert_config" json:"alert_config"`
+    AlertConfig alertConfig `db:"alert_config" json:"alert_config,omitempty"`
 
     // Primary contact for the account
-    AdminEmail string `db:"admin_email" json:"admin_email"`
+    AdminEmail string `db:"admin_email" json:"admin_email,omitempty"`
 }
 
 func (a Account) JSON() ([]byte, error) {
